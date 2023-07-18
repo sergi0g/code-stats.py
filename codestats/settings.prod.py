f1c6a5d7198da073e5144c80f6719b968dd5b64b
Potaot <77530549+Potaot@users.noreply.github.com>
2023-07-18 16:04:50 +0300

    Temporarily enable debug to check something

--- codestats/settings.prod.py
+++ codestats/settings.prod.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default="veryinsecure")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv("HOSTNAME", default="localhost")]
 
